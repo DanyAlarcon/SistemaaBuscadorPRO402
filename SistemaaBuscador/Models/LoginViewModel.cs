7eb6722f275b85eb3b4cffd33c605aa3ca6145fa
Dany <Andres@DESKTOP-4BN379M>
2021-10-16 06:11:30 -0300

    Revert "modificacion0:36"
    This reverts commit 7492090d552634425724639c937baaa50ef16729.

--- SistemaaBuscador/Models/LoginViewModel.cs
+++ SistemaaBuscador/Models/LoginViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FsCheck;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -11,7 +12,7 @@ namespace SistemaaBuscador.Models
         [Required(ErrorMessage ="El campo {0} es requerido")]
         public string Usuario { get; set; }
         [Display(Name="Contraseña")]
-        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Password { get; set; }
     }
 }
